b1a83ef831ea2be50e4dae30b3409c2de926a537
周世涛 <116058082+dademao@users.noreply.github.com>
2022-10-19 20:58:37 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,79 @@
+//
+//  main.c
+//  The beginning of my dream
+//
+//  Created by shitao zhou on 2022/10/19.
+//
+
+//#include <stdio.h>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+
+
+//#include <stdio.h>
+//int main ()
+//{
+//    int a , b;
+//    int t = 1 ;
+//    scanf ("%d %d",&a,&b);
+//
+//    while ( b != 0 )
+//    {
+//        t = a % b ;
+//        a = b;
+//        b = t;
+//    }
+//    printf ("%d\n",a);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a ;
+//    scanf ("%d",&a);
+//    int i,j,k;
+//    int cnt = 0;
+//    i = a ;
+//    while ( i <= a+3 ){
+//        j = a ;
+//        while ( j <= a+3 ){
+//            k = a ;
+//            while ( k <= a+3 ){
+//                if ( i != j ){
+//                    if ( i != k ){
+//                        if ( j != k ){
+//                            printf ("%d%d%d",i,j,k);
+//                            cnt ++;
+//                            if ( cnt == 6){
+//                                printf ("\n");
+//                                cnt = 0;
+//                            }
+//                            else{
+//                                printf(" ");
+//                            }
+//                        }
+//                    }
+//                }
+//                k ++;
+//            }
+//            j ++;
+//        }
+//        i ++;
+//    }
+//    return 0;
+//}
+//
+//#include<stdio.h>
+//int main ()
+//{
+//    int n;
+//    scanf("%d",&n);
+//    int i = 1;
+//    
+//    
+//}
